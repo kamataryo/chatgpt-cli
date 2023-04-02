d0991f902a4fca3777226223ab59b757fd1de2c4
kamataryo <6292312+kamataryo@users.noreply.github.com>
2023-04-02 14:43:58 +0900

    Add HTTP request

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use std::error::Error;
 use std::env;
 use std::fs;
 use std::path::Path;
 use std::process;
 use clap::Parser;
 use yaml_rust::YamlLoader;
+use reqwest;
+use serde::{Serialize, Deserialize};
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -15,17 +18,24 @@ struct Credential {
   openai_secret_key: String,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
 
   let credential = parse_credentials();
 
   let args = Args::parse();
-  let initial_query_text = args.initial_query_text;
-  println!("{}", initial_query_text);
+  let initial_query_text = args.initial_query_text.as_str();
 
+  let result = ask(initial_query_text, credential).await;
+
+  match result {
+    Ok(answer) => println!("{}", answer),
+    Err(err) => {},
+  }
 
 }
 
+/** Parse OpenAI credential from environment or .chatgpt-cli.yaml */
 fn parse_credentials() -> Credential {
     // 環境変数を確認
     let mut openai_organization_id = match env::var("OPENAI_ORGANIZATION_ID") {
@@ -102,7 +112,73 @@ fn parse_credentials() -> Credential {
   };
 }
 
-// TODO:
-// - login コマンド (org と API キーを保存)
-// - モデルは GPT-4 固定
-// -
+#[derive(Debug, Serialize, Deserialize)]
+struct ChatCompletionMessage {
+  role: String,
+  content: String,
+}
+
+#[derive(Debug, Serialize)]
+struct ChatCompletionsPostParams {
+  model: String,
+  messages: Vec<ChatCompletionMessage>,
+}
+
+#[derive(Debug, Deserialize)]
+struct ChatCompletionsResponseUsage {
+  prompt_tokens: i8,
+  completion_tokens: i8,
+  total_tokens: i8
+}
+
+#[derive(Debug, Deserialize)]
+struct ChatCompletionsResponseChoice {
+  message: ChatCompletionMessage,
+  finish_reason: String,
+  index: i8,
+}
+
+#[derive(Debug, Deserialize)]
+struct ChatCompletionsResponseParams {
+  id: String,
+  object: String,
+  created: i32,
+  model: String,
+  usage: ChatCompletionsResponseUsage,
+  choices: Vec<ChatCompletionsResponseChoice>,
+}
+
+async fn ask (content: &str, credential: Credential) -> Result<String, Box<dyn Error>> {
+  let client = reqwest::Client::new();
+
+  let post_message = ChatCompletionMessage {
+    role: "user".to_string(),
+    content: content.to_string(),
+  };
+  let mut messages = Vec::new();
+  messages.push(post_message);
+  let post_params = ChatCompletionsPostParams {
+    model: "gpt-3.5-turbo".to_string(),
+    messages: messages,
+  };
+
+  let post_body = serde_json::to_string(&post_params).unwrap();
+
+  let response_body = client.post("https://api.openai.com/v1/chat/completions")
+    // .header("OpenAI-Organization", credential.openai_organization_id)
+    .header("Content-Type", "application/json")
+    .header("Authorization",  format!("Bearer {}", credential.openai_secret_key))
+    .body(post_body)
+    .send()
+    .await?
+    .text()
+    .await?
+  ;
+  let response_params: ChatCompletionsResponseParams = serde_json::from_str(response_body.as_str()).unwrap();
+  let answer = &response_params.choices[0].message.content;
+
+  Ok(answer.to_string())
+}
+
+
+
